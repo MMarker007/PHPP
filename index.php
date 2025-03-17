e6710a50fa1e2afe0c01c40f9a8748ff93b7ca7d
MMarker007 <max2006grishin@gmail.com>
2025-03-18 02:01:17 +0300

    first save

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP_lab</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <header>
+        <img class='header_logo' src='./img/mospolytech_logo.png' alt='Логотип'>
+        <h2>Moscow Polytech</h2>
+    </header>
+    <main>
+        <h1>Hello world</h1>
+        <h2>my name is Maksim</h2>
+    </main>
+    <footer>
+        <h1> задание для самостоятельно работы</h1>
+    </footer>
+</body>
+</html>
